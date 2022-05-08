55efb0744893b537435cc0bae3a38aa1f2ccc98a
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-08 12:05:27 +0530

    AnalyseMood

---
+++ Mood_Analyzer_problem/MoodAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Mood_Analyzer_problem
+{
+    public enum Mood
+    {
+        Happy,
+        sad
+    }
+
+    public class MoodAnalyzer
+    {
+        public string CheckMood(string msg)
+        {
+            //msg : Rishabh is Happy for his new laptop
+            //msg : Rishabh is Sad after getting bad marks
+            if (msg.Contains("happy", StringComparison.OrdinalIgnoreCase))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+
+
+
+}
