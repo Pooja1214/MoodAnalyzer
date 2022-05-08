3d050f7bf0672a26e15074dbdd50f005c4f95763
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-08 12:16:48 +0530

    ReflectionForMoodAnalyser

---
+++ Mood_Analyzer_problem/FactoryMood.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer_problem
+{
+    public class MoodAnalyzerFactory
+    {
+        // CreateMoodAnalyse method to create object of MoodAnalyser class.
+        public static Object CreateMoodAnalyze(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new CustomMoodAnalyzerException(CustomMoodAnalyzerException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new CustomMoodAnalyzerException(CustomMoodAnalyzerException.ExceptionType.NO_SUCH_METHOD, "Constructor is not Found");
+            }
+        }
+    }
+}
